8a69c1ab660f3f3fbd037f6e020c153a56270802
Andrew Davis <andrewdavis@Andrews-MacBook-Air-78.local>
2023-05-02 09:09:12 -0400

    added final project folder

---
+++ fp/createconda.py
@@ -0,0 +1,81 @@
+""" IUB - L715NNs/Nahuatl Spring 2023 - Final Project - Andrew Davis - How to create a MiniConda Environment:
+
+In Google Colab (Per Chat GPT & it works):
+
+Sure, here are the steps to install and use Miniconda3 in Google Colab:
+
+    First, you need to download the Miniconda installer script for Linux from the official website. You can download it using the following command:
+
+    arduino
+
+!wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+Next, you need to run the installer script by running the following command:
+
+bash
+
+!bash Miniconda3-latest-Linux-x86_64.sh -bfp /usr/local
+
+This will install Miniconda in the /usr/local directory of the Colab environment.
+
+Once Miniconda is installed, you can add it to your system path by running the following command:
+
+go
+
+import sys
+sys.path.append('/usr/local/lib/python3.7/site-packages')
+
+This will allow you to use the conda command in your Colab notebook.
+
+Finally, you can create a new Conda environment by running the following command:
+
+lua
+
+!conda create --name env_name
+
+Replace env_name with the name you want to give to your environment.
+
+Once the environment is created, you can activate it by running the following command:
+
+diff
+
+    !conda activate env_name
+
+    Now you are inside the environment and can install the required packages using Conda or pip.
+
+Note: Make sure to restart the runtime of your Colab notebook after running the above commands to apply the changes.
+
+**************
+
+Conda environment Via Rob P
+
+Carbonate Option:
+
+!wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+bash Miniconda3-latest-Linux-x86_64.sh
+
+then once you o through the prompts to install conda, create a new env with:
+conda create -n <environment name> python=3.8
+
+
+then:
+conda activate <environment name>
+
+^if that doesn't work , check path 
+
+cat ~/.bash_profile 
+ 
+Solve with :
+source ~/.bashrc
+
+And check with:
+conda
+
+
+I now have a Conda node that I can use for Nahuatl on the GPU, nn.final.venv and It has opennmt in it 
+
+(base) [zleech@h2 miniconda3]$ conda activate nn.final.venv
+(nn.final.venv) [zleech@h2 miniconda3]$ pip install OpenNMT-py (activate (no longer need to install))
+
+"""
---
+++ fp/preproc.py
@@ -0,0 +1,63 @@
+""" NNs/Nahuatl Spring 2023 Final Project Andrew Davis -- Preprocessing Get all Source & Target Tokenized Sentences """
+
+""" 
+Yes, you can search for the same things in a CoNLL-U file using regular expressions.
+
+CoNLL-U files are tab-separated text files that contain one token per line, with columns representing different annotations like 
+the word form, lemma, part-of-speech tag, and syntactic dependency relations. The text and text[orig] attributes are typically not 
+included in CoNLL-U files, but you can extract the corresponding word forms using the FORM column.
+
+Here's an example Python code that demonstrates how to extract the sentences that come after text = or text[orig] = in a 
+CoNLL-U file: """
+
+#Step 1: Open the conllu file from FC Book and make an out txt file for the token versions of text and text[orig]
+import re
+
+filename = "Book_08_-_Kings_and_Lords.conllu"  # Replace with your CoNLL-U file name
+OUT = open('toke-src-8.txt', 'w')
+
+#Step 2: Define the regular expression pattern to match text after 'text =' or 'text[orig] ='
+
+#pattern = r'(?:text|text\[orig\])\s*=\s*([^\n]+)'
+textog_pattern = r'(?:text\[orig\])\s*=\s*([^\n]+)'
+#text_pattern = r'(?:text)\s*=\s*([^\n]+)'
+
+#Step 3: Open the file and read its contents
+
+with open(filename, 'r') as file:
+    contents = file.read()
+
+#Step 4: Use regular expressions to extract the sentences that match the pattern(s)
+
+sentences = []
+for match in re.finditer(textog_pattern, contents):
+    sentence = match.group(1).strip()
+    sentences.append(sentence)
+
+#Step 5: Process the extracted sentences to insert spaces between each char and to create a newline after punctuation
+
+processed_sentences = []
+for sentence in sentences:
+    sentence = ''.join(sentence.split())  # Remove whitespace
+    processed_sentence = ''
+    for i, char in enumerate(sentence):
+        if i > 0 and char in '.?!:':  # Check for punctuation
+            processed_sentence += char + '\n'
+        else:
+            processed_sentence += char + ' '
+    processed_sentences.append(processed_sentence)
+
+#Step 6: Print the extracted sentences and store them in a txt file for openNMT
+
+for sentence in processed_sentences:
+    print(sentence)
+    OUT.write(sentence)
+
+OUT.close()
+
+""" This code first extracts the substrings that match the regular expression pattern using re.finditer(). 
+It then iterates over each match and extracts the corresponding word forms using the FORM column. 
+The extracted word forms are then joined together to form a sentence.
+
+Note that this code assumes that the CoNLL-U file follows the standard format with the FORM column as the second column. 
+If your file has a different format, you may need to modify the code to extract the word forms from the correct column. """
